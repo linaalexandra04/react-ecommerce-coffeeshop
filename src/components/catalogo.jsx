bf5bacfea62e180bd3620b123196208ed8261023
lina alexandra <lennaalexandra.1999@gmail.com>
2024-06-08 20:11:33 -0500

    add: cambios

--- src/components/catalogo.jsx
+++ src/components/catalogo.jsx
@@ -2,13 +2,32 @@ import React from 'react';
 import '../styles/catalogo.css'
 
 const Catalogo = () => {
-return (
-    <div>
-    <h2>Catálogo</h2>
-    <p>Aquí se muestra el catálogo de productos.</p>
-    </div>
-);
-};
+    const images = [
+    '/img/cafe1.png',
+    '/img/cafe2.png',
+    '/img/cafe3.png',
+    '/img/cafe4.png',
+    '/img/cafe5.png',
+    '/img/cover2.png',
+    '/img/cover3.png',
+    '/img/cover4.png',
+    '/img/cover5.png',
+    ];
+
+    return (
+        <div className="catalogo">
+          <h2 className="catalogo-title">Catálogo</h2>
+          <p className="catalogo-description">Café Gourmet.</p>
+          <div className="gallery">
+            {images.map((image, index) => (
+              <div key={index} className="gallery-item">
+                <img src={image} alt={`Gallery item ${index + 1}`} />
+              </div>
+            ))}
+          </div>
+        </div>
+      );
+    };
 
 
 export default Catalogo;
